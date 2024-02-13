8e8e1ae98409fc3c98b783f2ef9975220576faf8
Emilia <voxelost@proton.me>
2024-02-13 01:25:53 +0100

    add simple dumb caching

--- src/lib.rs
+++ src/lib.rs
@@ -9,13 +9,21 @@ pub async fn main(req: worker::Request, env: Env, _ctx: worker::Context) -> work
 
     Router::new()
         .get_async("/:query", |_req, ctx| async move {
-            let query = urlencoding::decode(ctx.param("query").unwrap()).unwrap().into_owned();
-            console_log!("querying {}", query);
+            let query = ctx.param("query").unwrap();
+            let cache = ctx.kv("OPENFOODS_CACHE").unwrap();
+            let query_urldecoded = urlencoding::decode(query).unwrap().into_owned();
+
+            if let Some(cached_result) = cache.get(&query).text().await.unwrap() {
+                console_log!("found a cached result for query {}", query_urldecoded);
+                return worker::Response::ok(cached_result);
+            }
+
+            console_log!("querying {}", query_urldecoded);
 
             if let Ok(response) = reqwest::Client::new()
                 .get("https://world.openfoodfacts.org/cgi/search.pl")
                 .query(&[
-                    ("search_terms", query),
+                    ("search_terms", query_urldecoded),
                     ("search_simple", String::from("1")),
                     ("action", String::from("process")),
                 ])
@@ -23,7 +31,10 @@ pub async fn main(req: worker::Request, env: Env, _ctx: worker::Context) -> work
                     let html: &str = &response.text().await.unwrap()[..];
                     let products = utils::extract_products(html);
                     if products.is_ok() {
-                        return worker::Response::ok(products.unwrap());
+                        console_log!("found products, inserting into cache");
+                        let products_json = products.unwrap();
+                        cache.put(&query, products_json).unwrap().execute().await.unwrap();
+                        return worker::Response::ok(products_json);
                     } else {
                         let mut out = products.unwrap_err().to_string();
                         out.push_str(&format!("\nopenfoodfacts.org server returned:\n{}", html)[..]);
